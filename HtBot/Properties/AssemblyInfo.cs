a327070d0e766c8573aa6b96c56f425fba7b1e6c
Bezouro <bbezouro@gmail.com>
2019-05-09 16:43:59 -0300

    Update

--- HtBot/Properties/AssemblyInfo.cs
+++ HtBot/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Minecraft Console Client")]
-[assembly: AssemblyDescription("Lightweight console for Minecraft chat and automated scripts")]
+[assembly: AssemblyTitle("HtBot")]
+[assembly: AssemblyDescription("Bot do telegram baseado no ORelio - MCC")]
 //[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://github.com/ORelio/Minecraft-Console-Client")]
-[assembly: AssemblyProduct("MinecraftClient")]
-[assembly: AssemblyCopyright("Copyright © 2012-2018 ORelio & Contributors")]
+[assembly: AssemblyCompany("Bezouro")]
+[assembly: AssemblyProduct("HtBot")]
+[assembly: AssemblyCopyright("Copyright © 2018 Bezouro")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-// [assembly: AssemblyVersion("1.0.0")]
+ [assembly: AssemblyVersion("1.2.2")]
 // [assembly: AssemblyFileVersion("1.0.0")]
 
 // AppVeyor Build Number
